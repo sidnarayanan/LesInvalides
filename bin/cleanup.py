f07e5218a8cfb96568bf553f68f47692dd854b76
Sid Narayanan <sidn@mit.edu>
2016-12-28 02:29:56 -0500

    non-test executables

---
+++ bin/cleanup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import mymysql
+import invalidation
+
+mymysql.connect(db='testdb')
+mymysql.set_table()
+to_remove = mymysql.select_rows(condition='executed=0',columns='task_id,lfn,data')
+rets = {0:0,1:0,2:0}
+n_removed = 0
+n_requested = len(to_remove)
+n_data = 0
+
+for r in to_remove:
+  lfn = r[1]
+  data = r[2]
+  if not data:
+    retcode = invalidation.invalidate(lfn)
+    rets[retcode] += 1
+    if retcode==0:
+      n_removed += 1
+      mymysql.mark_executed(lfn)
+  else:
+    n_data += 1
+mymysql.commit()
+
+print 'SUMMARY'
+print '\tNumber of files removed:       %i'%n_removed
+print '\tNumber of files left in limbo: %i'%(n_requested-n_removed)
+print '\tNumber of data files:          %i'%(n_data)
---
+++ bin/request.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import mymysql
+import argparse
+from os import path
+
+parser = argparse.ArgumentParser(description='request the invalidation of files')
+parser.add_argument('--description',metavar='description',type=str)
+parser.add_argument('files',metavar='files',nargs='+',type=str)
+args = parser.parse_args()
+
+if len(args.files)==1 and path.isfile(args.files[0]):
+  with open(args.files[0]) as infile:
+    file_list = list(infile)
+else:
+  file_list = args.files
+
+mymysql.connect(db='testdb')
+mymysql.set_table()
+for f in file_list:
+  is_data = ('/store/data' in f)
+  mymysql.insert_request(f,is_data,args.description)
+mymysql.commit()
+mymysql.close()
